518fdd18d2e9322d4bfe08d4f6f7ccbd2d92c17e
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-09 23:18:40 +0500

    navbar mobile version

--- src/components/navbar/mobileNav/mobileNav.jsx
+++ src/components/navbar/mobileNav/mobileNav.jsx
@@ -1,22 +1,14 @@
-import React, { useState } from 'react'
-import './Navbar.css'
+import React from 'react'
+import './mobileNav.css'
 import { TbDeviceDesktopCode } from "react-icons/tb";
-import { MdMenu } from "react-icons/md";
-import MobileNav from './mobileNav/mobileNav';
-import { IoCloseSharp } from "react-icons/io5";
-const Navbar = () => {
-    const [openMenu, setOpenMenu] = useState(false)
 
-    const toggleMenu = () => {
-        setOpenMenu(!openMenu)
-    }
+const MobileNav = ({ isOpen, toggleMenu }) => {
     return (
         <>
-            <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
-            <div className='nav-wrapper'>
-                <div className="nav-content">
+            <div className={`mobile-menu ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                <div className="mobile-menu-container">
                     <div className='logo-wrapper'>
-                        <TbDeviceDesktopCode className='logo' />
+                        <TbDeviceDesktopCode className='logo' size={40} />
                         <h3>frdevv17</h3>
                     </div>
                     <ul>
@@ -34,13 +26,10 @@ const Navbar = () => {
                         </li>
                         <button className='contact-btn' onClick={() => { }}>Hire Me</button>
                     </ul>
-                    <button class='menu-btn' onClick={toggleMenu}>
-                        {!openMenu ? <MdMenu size={30} /> : <IoCloseSharp size={30} />}
-                    </button>
                 </div>
             </div>
         </>
     )
 }
 
-export default Navbar
+export default MobileNav
